7df4957de885776dbb337fdc95221de26e09cd0a
TheRandomTroll <mihailgerginov01@gmail.com>
2019-11-05 21:15:48 -0500

    Added MJPEG decomposer

---
+++ crime-spy-server/mock-server.py
@@ -0,0 +1,19 @@
+import requests
+
+r = requests.get('http://192.168.43.1:3000/video', stream=True)
+if(r.status_code == 200):
+    i = 1
+    bytes = bytes()
+    f = open('test{}.jpg'.format(i), 'wb')
+    for chunk in r.iter_content(chunk_size=1024):
+        bytes += chunk
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            f.write(bytes[a:b+2])
+            f.close()
+            bytes = bytes[b+2:]
+            i+=1
+            f = open('test{}.jpg'.format(i), 'wb')
+else:
+    print("Received unexpected status code {}".format(r.status_code))
